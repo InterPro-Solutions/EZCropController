5d81024cb45302f9b803f92341eb71f86806c266
Xiang Li <leexiang114@gmail.com>
2021-04-09 10:40:30 -0400

    New : add Swift and Objective-C example iOS project

---
+++ EZCropExample/EZCropExample/OC_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZCropExample_OC
+//
+//  Created by Xiang Li on 4/8/21.
+//
+
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
