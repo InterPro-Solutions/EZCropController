42a47817d268a20941450820dea17fbe33b95f12
Xiang Li <leexiang114@gmail.com>
2021-04-27 09:59:07 -0400

    New: set swift language version to v5

--- Package.swift
+++ Package.swift
@@ -25,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "EZCropControllerTests",
             dependencies: ["EZCropController"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
